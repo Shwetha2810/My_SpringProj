a898bcc1d46d1f324425781943b3b0007a57f131
Shwetha2810 <shwethamogaveera01@gmail.com>
2025-11-26 11:28:03 +0530

    Delete E-Commerce_Platform-master/E-Commerce_Platform-master directory

--- E-Commerce_Platform-master/E-Commerce_Platform-master/src/main/java/com/m15/clicknbuy/repository/OrderItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.m15.clicknbuy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.m15.clicknbuy.entity.OrderItem;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
